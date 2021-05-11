54452556b050501fa94eed696d1bf17db3227348
Dave Moran <dave.moran118@gmail.com>
2021-05-11 11:26:29 -0400

    Add new view

--- week-3/lecture3/hello/views.py
+++ week-3/lecture3/hello/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Hello, world!")
+  return HttpResponse("Hello!")
+
+def brian(request):
+  return HttpResponse("Hello, Brian!")
